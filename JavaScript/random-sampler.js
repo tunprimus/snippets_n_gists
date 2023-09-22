68ad88d8f7f1c6888aa28a95263f983e9474692b
T Ol <31147296+tunprimus@users.noreply.github.com>
2023-09-22 11:09:04 +0100

    feat: dynamically generate the count test variable

--- JavaScript/random-sampler.js
+++ JavaScript/random-sampler.js
@@ -9,28 +9,34 @@ const getRandomItem = (items) => items[Math.floor(Math.random() * items.length)]
 
 // Test variables
 const testArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',];
-const counts = {a: 0, b: 0, c: 0, d: 0, e: 0, f: 0, g: 0, h: 0, i: 0, j: 0,};
+const counts = testArray.reduce((acc, cur) => {
+  acc[cur] = 0;
+  return acc;
+}, {});
+// console.log(counts);
+// const counts2 = {a: 0, b: 0, c: 0, d: 0, e: 0, f: 0, g: 0, h: 0, i: 0, j: 0,};
+
 let randomItem;
-const simulations = 99_000_000;
+const SIMULATIONS = 99_000_000;
 
 // Validation
-for (let i = 0; i < simulations; i++) {
+for (let i = 0; i < SIMULATIONS; i++) {
   randomItem = getRandomItem(testArray);
   counts[randomItem]++;
 }
 
 // Result presentation
 const proportions = {
-  a: counts.a / simulations,
-  b: counts.b / simulations,
-  c: counts.c / simulations,
-  d: counts.d / simulations,
-  e: counts.e / simulations,
-  f: counts.f / simulations,
-  g: counts.g / simulations,
-  h: counts.h / simulations,
-  i: counts.i / simulations,
-  j: counts.j / simulations,
+  a: counts.a / SIMULATIONS,
+  b: counts.b / SIMULATIONS,
+  c: counts.c / SIMULATIONS,
+  d: counts.d / SIMULATIONS,
+  e: counts.e / SIMULATIONS,
+  f: counts.f / SIMULATIONS,
+  g: counts.g / SIMULATIONS,
+  h: counts.h / SIMULATIONS,
+  i: counts.i / SIMULATIONS,
+  j: counts.j / SIMULATIONS,
 };
-console.log(proportions);
+// console.log(proportions);
 console.table(proportions);
