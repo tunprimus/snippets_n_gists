908089f7c89cff11e8fc18d7e33a973816fc28a9
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-05-20 17:48:14 +0100

    feat: Add a recur

---
+++ JavaScript/recursive-filter.js
@@ -0,0 +1,70 @@
+//@ts-check
+
+function log(item) {
+	console.log(JSON.stringify(item));
+}
+
+const people = [
+	{
+		age: 2,
+	},
+	{
+		age: 22,
+		friends: [
+			{
+				age: 17,
+			},
+			{
+				age: 34,
+			},
+			{
+				name: "Sherry O'Teri",
+			}
+		],
+	},
+	{
+		name: 'Bob Ross',
+	},
+	{
+		age: 12,
+		friends: [
+			{
+				age: 44,
+			},
+		],
+	}
+];
+
+function validAge(item) {
+	return typeof item.age === 'number' && item.age > 17;
+}
+
+function thisWontWorkRecursiveFilter(arr) {
+	return arr.filter(item => {
+		if (item.friends) {
+			// Here is the recursive call
+			return (thisWontWorkRecursiveFilter(item.friends).length > 0 && validAge(item));
+		} else {
+			return validAge(item);
+		}
+	});
+}
+log(thisWontWorkRecursiveFilter(people));
+
+function recursiveFilter(arr) {
+	return arr.reduce((acc, item) => {
+		const newItem = item;
+
+		if (item.friends) {
+			// Here is the recursive call
+			newItem.friends = recursiveFilter(item.friends);
+		}
+		if (validAge(newItem)) {
+			// Here acc takes the new item
+			acc.push(newItem);
+		}
+		// Always return acc
+		return acc;
+	}, []);
+}
+log(recursiveFilter(people));
