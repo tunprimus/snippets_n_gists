2d4fb2f6a964f773a8df7813522b0e651a64a449
T Ol <31147296+tunprimus@users.noreply.github.com>
2024-11-08 06:50:50 +0100

    feat: Add csv to insert statements generator

---
+++ Python/convert_csv_to_sql_insert_statement.py
@@ -0,0 +1,27 @@
+# Adapted from: Convert an Excel dataset into a SQL insert statement
+# https://dev.to/smuniraj/convert-excel-dataset-into-sql-insert-statement-38k4
+import pandas as pd
+from os.path import realpath as realpath
+
+
+def csv_to_sql_insert(path_to_csv, path_to_sql_output=".", table_name="Test_Table_Name"):
+    real_path_to_csv = realpath(path_to_csv)
+    real_path_to_sql_output = realpath(path_to_sql_output)
+    # Generate SQL insert statements
+    table_name = table_name
+    sql_statements = []
+
+    df = pd.read_csv(real_path_to_csv)
+
+    for index, row in df.iterrows():
+        columns = ", ".join(row.index)
+        values = ", ".join([f"'{str(value)}'" for value in row.values])
+        sql_statements.append(f"INSERT INTO {table_name} ({columns}) VALUES ({values});")
+
+    # Save the SQL statements to a file
+    with open(f"{real_path_to_sql_output}/insert_statements.sql", "w") as fop:
+        for statement in sql_statements:
+            fop.write(statement + "\n")
+
+if __name__ == "__main__":
+    csv_to_sql_insert()
