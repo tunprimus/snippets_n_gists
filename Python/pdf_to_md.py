c6a3a308bae8f490b456bc2aeb4c064f4cbbf01b
T Ol <31147296+tunprimus@users.noreply.github.com>
2025-05-26 15:47:01 +0100

    modified:   Python/pdf_to_md.py

--- Python/pdf_to_md.py
+++ Python/pdf_to_md.py
@@ -11,10 +11,24 @@ def pdf_to_markdown(input_file, output_file="test-output.md"):
     real_path_to_input_file = realpath(input_file)
     real_path_to_output_file = realpath(output_file)
 
-    #md_text_with_tables = pymupdf4llm.to_markdown(doc=real_path_to_input_file,  page_chunks=True, write_images=True, image_path="./converted_pdf_to_markdown", image_format="png", dpi=300, extract_words=True)
-    md_text_with_tables = pymupdf4llm.to_markdown(doc=real_path_to_input_file)
-    #pathlib.Path(f"{output_file}").write_bytes(md_text_with_tables.encode())
-    pathlib.Path(real_path_to_output_file).write_bytes(md_text_with_tables.encode())
+    md_text_with_tables = pymupdf4llm.to_markdown(doc=real_path_to_input_file,  page_chunks=True, write_images=True, image_path="./converted_pdf_to_markdown", image_format="png", dpi=300, extract_words=True)
+    #md_text_with_tables = pymupdf4llm.to_markdown(doc=real_path_to_input_file)
+    pathlib.Path(f"{output_file}").write_bytes(md_text_with_tables.encode())
+    #pathlib.Path(real_path_to_output_file).write_text(md_text_with_tables)
+    """
+    TODO:
+    Fix error:
+    Traceback (most recent call last):
+        File "pdf_to_md.py", line 49, in <module>
+            main(pdf_path)
+        File "pdf_to_md.py", line 41, in main
+            pdf_to_markdown(real_path_to_pdf, real_path_to_output)
+        File "pdf_to_md.py", line 17, in pdf_to_markdown
+            pathlib.Path(real_path_to_output_file).write_text(md_text_with_tables)
+        File "/usr/lib/python3.8/pathlib.py", line 1253, in write_text
+            raise TypeError('data must be str, not %s' %
+        TypeError: data must be str, not list
+    """
     print("Markdown saved to output file")
 
 def main(path_to_pdf, output_dir="./converted_pdf_to_markdown"):
@@ -29,7 +43,7 @@ def main(path_to_pdf, output_dir="./converted_pdf_to_markdown"):
 if __name__ == "__main__":
     import sys
     if len(sys.argv) < 2:
-        print("Usage: python pdf_to_md.py <path_to_pdf>")
+        print("Usage: python3 pdf_to_md.py <path_to_pdf>")
         sys.exit(1)
     pdf_path = sys.argv[1]
     main(pdf_path)
