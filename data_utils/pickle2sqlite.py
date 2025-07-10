11dfb8718dabe84c4325a0550a763da9253bff4e
T Ol <31147296+tunprimus@users.noreply.github.com>
2025-07-10 16:04:01 +0100

    fix: Make retrieving Python version better

--- data_utils/pickle2sqlite.py
+++ data_utils/pickle2sqlite.py
@@ -2,43 +2,44 @@
 import sqlite3
 import pickle
 import codecs
-import sys.version as version
+import sys
 
 from os.path import expanduser, realpath
 
 
-python_version = version
+python_version = sys.version.split(" ")[0].replace(".", "_")
+
 """
 Adapted from https://gist.github.com/molpopgen/aa6225a18466591213880d320748f9bc
 """
 def to_pickle_encoded(filename):
     """
     Read a file and encode it using base64 for storage in a sqlite database.
-    
+
     Parameters
     ----------
     filename : str
         The path to the file to read.
-    
+
     Returns
     -------
     str
         The base64 encoded string.
     """
     filename = realpath(expanduser(filename))
-    
+
     with open(filename, "rb") as fin:
         to_dump = pickle.dumps(fin, -1)
         filename_encoded = codecs.encode(to_dump, "base64").decode()
         filename_encoded = f"{filename_encoded}-{python_version}"
-    
+
     return filename_encoded
 
 
 def save_pickle_to_sqlite(pickled_file, db_name="test.db", id_val=1):
     """
     Save a pickled file to a sqlite database.
-    
+
     Parameters
     ----------
     pickled_file : str
@@ -47,14 +48,14 @@ def save_pickle_to_sqlite(pickled_file, db_name="test.db", id_val=1):
         The name of the database to store the file in.
     id_val : int
         The id to use for the insert statement.
-    
+
     Returns
     -------
     int
         The id of the inserted row.
     """
     real_path_to_db = realpath(expanduser(db_name))
-    
+
     with sqlite3.connect(real_path_to_db) as conn:
         conn.execute("create table pickle_store (id integer, pickled text, python_version text)")
         conn.execute(
@@ -68,21 +69,21 @@ def save_pickle_to_sqlite(pickled_file, db_name="test.db", id_val=1):
 def load_pickle_from_sqlite(db_name="test.db", id_val=1):
     """
     Load a pickled file from a sqlite database.
-    
+
     Parameters
     ----------
     db_name : str
         The name of the database to load the file from.
     id_val : int
         The id to use for the select statement.
-    
+
     Returns
     -------
     object
         The unpickled object.
     """
     real_path_to_db = realpath(expanduser(db_name))
-    
+
     with sqlite3.connect(real_path_to_db) as conn:
         cursor = conn.cursor()
         cursor.execute("select pickled from pickle_store where id == id_val")
