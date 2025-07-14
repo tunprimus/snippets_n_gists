9d08f0c59729db7a2e5011065f6fc70e2977a190
T Ol <31147296+tunprimus@users.noreply.github.com>
2025-07-15 00:35:16 +0100

    modified:   Python/timing_function.py

--- data_utils/timing_function.py
+++ data_utils/timing_function.py
@@ -3,9 +3,10 @@
 from functools import wraps
 from time import time
 
-def timing_function(func):
-	from functools import wraps
-	from time import time
+def timing_function(func, show_full_params=False):
+    import time
+    from functools import wraps
+
     @wraps(func)
     def wrapper(*args, **kwargs):
         start_time = time.perf_counter()
@@ -13,13 +14,16 @@ def timing_function(func):
         result = func(*args, **kwargs)
         end_time = time.perf_counter()
         end_time_ns = time.perf_counter_ns()
-        
+
         execution_time = end_time - start_time
         execution_time_ns = end_time_ns - start_time_ns
-        
-        print(f"Function: {func.__name__} | Args: {args} | Took: {execution_time:.3f} seconds")
-        print(f"Function: {func.__name__} | Args: {args} | Took: {execution_time_ns} ns ({(execution_time_ns / 1000000000):.3f} s)")
-        
+
+        if show_full_params:
+            print(f"Function: {func.__name__} | Args: {args} | Took: {execution_time:.3f} seconds")
+            print(f"Function: {func.__name__} | Args: {args} | Took: {execution_time_ns} ns ({(execution_time_ns / 1000000000):.3f} s)")
+        else:
+            print(f"Function took: {execution_time_ns} ns ({(execution_time_ns / 1000000000):.3f} s)")
+
         return result
     return wrapper
 
