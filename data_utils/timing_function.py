3ecf7929fd7433920349dcade6d6533aa8287767
T Ol <31147296+tunprimus@users.noreply.github.com>
2025-07-15 08:49:42 +0100

    docs: Add documentation to timing_function

--- data_utils/timing_function.py
+++ data_utils/timing_function.py
@@ -4,6 +4,34 @@ from functools import wraps
 from time import time
 
 def timing_function(func, show_full_params=False):
+    """
+    A decorator that measures and prints the execution time of a function.
+
+    Parameters
+    ----------
+    func : callable
+        The function to be timed.
+    show_full_params : bool, optional
+        If True, prints the function name and arguments along with the timing information. Defaults to False.
+
+    Returns
+    -------
+    callable
+        A wrapped version of the input function that prints its execution time.
+
+    Notes
+    -----
+    The execution time is measured in both seconds and nanoseconds.
+    If `show_full_params` is True, the function name and arguments are also displayed.
+
+    Example
+    -------
+    >>> from timing_function import timing_function
+    >>>
+    >>> @timing_function
+    >>> def test_function():
+    >>>     print("Hello World!" * 13)
+    """
     import time
     from functools import wraps
 
@@ -22,7 +50,7 @@ def timing_function(func, show_full_params=False):
             print(f"Function: {func.__name__} | Args: {args} | Took: {execution_time:.3f} seconds")
             print(f"Function: {func.__name__} | Args: {args} | Took: {execution_time_ns} ns ({(execution_time_ns / 1000000000):.3f} s)")
         else:
-            print(f"Function took: {execution_time_ns} ns ({(execution_time_ns / 1000000000):.3f} s)")
+            print(f"Function: {func.__name__} took --> {execution_time_ns} ns ({(execution_time_ns / 1000000000):.3f} s)")
 
         return result
     return wrapper
